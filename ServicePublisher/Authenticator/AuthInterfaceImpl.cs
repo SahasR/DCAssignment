4047c3ffa5faad657c7e2551e018c7680b14d82a
harshi bandaranayke <harshi.bandaranayake@student.curtin.edu.au>
2022-09-16 00:37:25 +0530

    16/09/2022

---
+++ ServicePublisher/Authenticator/AuthInterfaceImpl.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authenticator
+{
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, UseSynchronizationContext = false)]
+    internal class AuthInterfaceImpl : AuthInterface
+    {
+        public AuthInterfaceImpl() { }
+
+        public String Register(String name, String password)
+        {
+            WriteFile(name, password);
+            return "Successfully registered!";
+        }
+
+        public int Login(String name, String password)
+        {
+            if (ReadFile(name, password))
+            {
+                Random random = new Random();
+                int number = random.Next();
+                return number;
+            }
+
+            /*EXCEPTION*/
+            return -1;
+        }
+
+        public String Validate(int token)
+        {
+            return "Validated";
+        }
+
+        private bool ReadFile(String name, String password)
+        {
+            bool isFound = false;
+
+            using (var reader = new StreamReader(@"C:\AuthInfo.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    if (name.Equals(values[0]) && password.Equals(values[1]))
+                    {
+                        isFound = true;
+                        break;
+
+                    }
+                }
+            }
+
+            return isFound;
+        }
+
+        /*SAVES NAME AND PASSWORD INTO A .CSV FILE*/
+        private void WriteFile(String name, String password)
+        {
+            using (var reader = new StreamWriter(@"C:\AuthInfo.csv"))
+            {
+
+            }
+        }
+    }
+}
---
+++ ServicePublisher/Authenticator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authenticator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host;
+            NetTcpBinding tcp = new NetTcpBinding();
+            host = new ServiceHost(typeof(AuthInterfaceImpl));
+            host.AddServiceEndpoint(typeof(AuthInterface), tcp, "net.tcp://0.0.0.0:8100/AuthenticatorService");
+            host.Open();
+            Console.WriteLine("Authenticator is Online");
+            Console.ReadLine();
+            host.Close();
+        }
+    }
+}
