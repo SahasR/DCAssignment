dd07e83cc05957adcfd20951c630291c7719a0fe
sahas <sahasrenuja@gmail.com>
2022-09-21 12:53:44 +0530

    Added the ServicePublisher with the Controllers

---
+++ ServicePublisher/ServiceProvider/Controllers/ThreeNumbersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ServiceProvider.Controllers
+{
+    [RoutePrefix("ThreeNumbers")]
+    public class ThreeNumbersController : ApiController
+    {
+        [Route("add/{firstNumber}/{secondNumber}/{thirdNumber}")]
+        [Route("add")]
+        [HttpGet]
+        public int Add(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            return firstNumber + secondNumber + thirdNumber;
+        }
+
+        [Route("multiply/{firstNumber}/{secondNumber}/{thirdNumber}")]
+        [Route("multiply")]
+        [HttpGet]
+        public int Multiply(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            return firstNumber * secondNumber * thirdNumber;
+        }
+    }
+}
---
+++ ServicePublisher/ServiceProvider/Controllers/TwoNumbersController.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Helpers;
+using System.Web.Http;
+
+namespace ServiceProvider.Controllers
+{
+    [RoutePrefix("TwoNumbers")]
+    public class TwoNumbersController : ApiController
+    {
+        [Route("add/{firstNumber}/{secondNumber}")]
+        [Route("add")]
+        [HttpGet]
+        public int Add(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        [Route("multiply/{firstNumber}/{secondNumber}")]
+        [Route("multiply")]
+        [HttpGet]
+        public int Multiply(int firstNumber, int secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+    }
+}
