cf46aa8525f16fd486afb4a3630fb5a85b9749f6
sahas <sahasrenuja@gmail.com>
2022-09-24 11:21:29 +0530

    Working API Connection and placeholder drop down list.

--- ServicePublisher/ClientGUI/MainWindow.xaml.cs
+++ ServicePublisher/ClientGUI/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using Authenticator;
 using InstanceLibrary;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,6 +28,7 @@ namespace ClientGUI
     public partial class MainWindow : Window
     {
         private AuthInterface authenticator; private static string username; private static string password;
+        private static int currToken;
         public MainWindow()
         {
             InitializeComponent();
@@ -39,7 +42,7 @@ namespace ClientGUI
 
             GUIDisable();
 
-            Task<string> task = new Task<string>(AsyncTask);
+            Task<string> task = new Task<string>(Register);
             task.Start();
             string result = await task;
             System.Windows.Forms.MessageBox.Show(result, "Registration", MessageBoxButtons.OK, MessageBoxIcon.Information);
@@ -47,18 +50,29 @@ namespace ClientGUI
             GUIEnable();
         }
 
-        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        private async void LoginButton_Click(object sender, RoutedEventArgs e)
         {
             username = UserNameBox.Text;
             password = PasswordBox.Text;
 
             GUIDisable();
 
+            Task<int> task = new Task<int>(Login);
+            task.Start();
+            int result = await task;
 
+            TokenBox.Text = result.ToString();
             GUIEnable();
         }
 
-        private string AsyncTask()
+        private int Login()
+        {
+            int token = authenticator.Login(username, password);
+            currToken = token;
+            return token;
+        }
+
+        private string Register()
         {
             string validation = authenticator.Register(username, password);
             return validation;
@@ -80,6 +94,44 @@ namespace ClientGUI
             RegisterButton.IsEnabled = true;
             LoginButton.IsEnabled = true;
             Progressbar.IsIndeterminate = false;
+        }
+
+        private void searchButton_Click(object sender, RoutedEventArgs e)
+        {
+            RestClient restClient = new RestClient("http://localhost:57446/");
+            RestRequest restRequest = new RestRequest("Registry/search/" + currToken + "/" + SearchBox.Text);
+            RestResponse restResponse = restClient.Get(restRequest);
+
+            if (restResponse.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                System.Windows.Forms.MessageBox.Show("Token invalid (might be expired), login again", "Login", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            } else if (restResponse.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                System.Windows.Forms.MessageBox.Show("API Service Seems to be down", "Not Found", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            } else if (restResponse.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                System.Windows.Forms.MessageBox.Show("Worked!", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void getAllButton_Click(object sender, RoutedEventArgs e)
+        {
+            RestClient restClient = new RestClient("http://localhost:57446/");
+            RestRequest restRequest = new RestRequest("Registry/getall/" + currToken);
+            RestResponse restResponse = restClient.Get(restRequest);
+
+            if (restResponse.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                System.Windows.Forms.MessageBox.Show("Token invalid (might be expired), login again", "Login", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (restResponse.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                System.Windows.Forms.MessageBox.Show("API Service Seems to be down", "Not Found", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (restResponse.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                System.Windows.Forms.MessageBox.Show("Worked!", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
     }
 }
